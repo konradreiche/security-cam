d52de32361d5486e2126564614eb834c682cb5df
Konrad Reiche <konrad.reiche@gmail.com>
2012-09-01 23:19:06 +0200

    Implement RequestHandler
    Using the Tornado web framework seems more fitting than its
    simple HTTP server. For now it is possible to register a
    device via GET request.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- server/securitas/notifier.py
+++ server/securitas/notifier.py
@@ -1,23 +1,46 @@
 from gcm import GCM
 
-from tornado import httpserver, ioloop
+from tornado import httpserver, ioloop, web
+from tornado.web import RequestHandler
 
 import ConfigParser
 
-def register_identifier(request):
-        message = "HTTP Requested %s\n" % request.method
-        print message
-        request.finish()
+
+class MainHandler(RequestHandler):
+        
+        def initialize(self, api_key):
+                self.gcm = GCM(api_key)
+
+        def post(self): 
+
+                uri = self.request.uri
+                if (uri == '/register'):
+                        self.application.device = self.get_argument('registrationId')
+                        print 'Registering Device %s' % self.application.device
+                        
+        def get(self):
+
+                uri = self.request.uri
+                if (uri == '/alarm'):
+                        data = { 'message' : 'Alarm!' }
+                        device = self.application.device
+                        print 'Send Message to Device %s' % device
+                        self.gcm.json_request(registration_ids = device, data = data)
+
+                self.set_status(200)
+                self.finish()
 
 
-config = ConfigParser.ConfigParser()
-config.read('key.cfg')
+if __name__ == "__main__":
+        
+        config = ConfigParser.ConfigParser()
+        config.read('key.cfg')
 
-API_KEY = config.get('API', 'key')
-
-http_server = httpserver.HTTPServer(register_identifier)
-http_server.listen(3030)
-ioloop.IOLoop.instance().start()
-
-gcm = GCM(API_KEY)
-data = { 'message' : 'Alarm!' }
+        api_key = config.get('API', 'key') 
+        
+        application = web.Application([
+                (r'.*', MainHandler, dict(api_key = api_key)),
+                ])
+        
+        application.listen(3030)
+        ioloop.IOLoop.instance().start()
