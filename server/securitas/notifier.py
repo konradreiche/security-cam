232556fba327f94dd7c382058f57263cb6637be9
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-09-03 15:01:58 +0200

    Add process control
    The Motion process should be started and terminated from the Python server process.
    Hence I have added a class which stores the necessary information and GET request
    cases which handle the process creation and termination.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- server/securitas/notifier.py
+++ server/securitas/notifier.py
@@ -4,28 +4,54 @@ from tornado import httpserver, ioloop, web
 from tornado.web import RequestHandler
 
 import ConfigParser
+import subprocess
+import os
 
+class ProcessControl(object):
+        """Stores information to control the Motion process"""
+
+        def __init__(self):
+                """Initializing the required attributes"""
+                self.device = None
+                self.process = None
+                
 
 class MainHandler(RequestHandler):
         
-        def initialize(self, api_key):
-                self.gcm = GCM(api_key)
+        def initialize(self, gcm, control):
+                self.gcm = gcm
+                self.control = control
 
         def post(self): 
 
                 uri = self.request.uri
                 if (uri == '/register'):
-                        self.application.device = self.get_argument('registrationId')
+                        self.control.device = self.get_argument('registrationId')
                         print 'Registering Device %s' % self.application.device
+                elif (uri == '/unregister'):
+                        self.control.device = None
+                        print 'Unregistering device'
                         
         def get(self):
 
                 uri = self.request.uri
-                if (uri == '/alarm'):
+                if (uri == '/alarm' and self.control.device != None):
                         data = { 'message' : 'Alarm!' }
-                        device = self.application.device
+                        device = self.control.device
                         print 'Send Message to Device %s' % device
                         self.gcm.plaintext_request(registration_id = device, data = data)
+
+                elif (uri == '/start'):
+                        print 'Start detection..'
+                        if (self.control.process == None):
+                                process = subprocess.Popen(['motion', '-c', '/etc/motion/motion.conf'])
+                                self.control.process = process
+
+                elif (uri == '/stop'):
+                        print 'Shutting detection down..'
+                        if (self.control.process != None):
+                                self.control.process.terminate()
+                                self.control.process = None
 
                 self.set_status(200)
                 self.finish()
@@ -37,9 +63,11 @@ if __name__ == "__main__":
         config.read('key.cfg')
 
         api_key = config.get('API', 'key') 
+        gcm = GCM(api_key)
+        control = ProcessControl()
         
         application = web.Application([
-                (r'.*', MainHandler, dict(api_key = api_key)),
+                (r'.*', MainHandler, dict(gcm = gcm, control = control)),
                 ])
         
         application.listen(3030)
