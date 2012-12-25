49933c4bd6cf88d0cdd77e0c35b5d10209235bdb
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-12-25 01:53:19 +0100

    Format Python code for PEP 8 compliance
    PEP 8 is a style guide for Python code. By using Flake8 I have checked
    the whole code of the notifier Python script. The file is now PEP 8
    compliant.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- server/securitas/notifier.py
+++ server/securitas/notifier.py
@@ -1,90 +1,90 @@
 from gcm import GCM
 
-from tornado import httpserver, httpclient, ioloop, web
+from tornado import httpclient, ioloop, web
 from tornado.web import RequestHandler, StaticFileHandler
 
 import ConfigParser
 import subprocess
-import os
+
 
 class ProcessControl(object):
-        """Stores information to control the Motion process"""
+    """Stores information to control the Motion process"""
 
-        def __init__(self):
-                """Initializing the required attributes"""
-                self.device = None
-                self.process = None
-                
+    def __init__(self):
+        """Initializing the required attributes"""
+        self.device = None
+        self.process = None
+
 
 class MainHandler(RequestHandler):
-        
-        def initialize(self, gcm, control):
-                self.gcm = gcm
-                self.control = control
 
-        def post(self): 
+    def initialize(self, gcm, control):
+        self.gcm = gcm
+        self.control = control
 
-                uri = self.request.uri
-                print uri
-                if (uri == '/control/register'):
-                        self.control.device = self.get_argument('registrationId')
-                        print 'Registering Device %s' % self.control.device
-                elif (uri == '/control/unregister'):
-                        self.control.device = None
-                        print 'Unregistering device'
-                        
-        def get(self):
+    def post(self):
 
-                if (self.control.device is not None):
+        uri = self.request.uri
+        print uri
+        if (uri == '/control/register'):
+            self.control.device = self.get_argument('registrationId')
+            print 'Registering Device %s' % self.control.device
+        elif (uri == '/control/unregister'):
+            self.control.device = None
+            print 'Unregistering device'
 
-                        uri = self.request.uri
-                        if (uri.startswith('/control/alarm')):
+    def get(self):
 
-                                picturePath = self.get_arguments('picture')
-                                data = { 'picture' : picturePath[0] }
-                                device = self.control.device
-                                print 'Send Message to Device %s' % device
-                                self.gcm.plaintext_request(registration_id = device, data = data)
+        if (self.control.device is not None):
+            uri = self.request.uri
+            if (uri.startswith('/control/alarm')):
+                picturePath = self.get_arguments('picture')
+                data = {'picture': picturePath[0]}
+                device = self.control.device
+                print 'Send Message to Device %s' % device
+                self.gcm.plaintext_request(registration_id=device,
+                                           data=data)
 
-                        elif (uri == '/control/start'):
-                                print 'Start detection..'
-                                if (self.control.process == None):
-                                        process = subprocess.Popen(['motion', '-c', '/etc/motion/motion.conf'])
-                                        self.control.process = process
+            elif (uri == '/control/start'):
+                print 'Start detection..'
+                if (self.control.process is None):
+                    process = subprocess.Popen(['motion', '-c',
+                                                '/etc/motion/motion.conf'])
+                    self.control.process = process
 
-                        elif (uri == '/control/stop'):
-                                print 'Shutting detection down..'
-                                if (self.control.process != None):
-                                        self.control.process.kill()
-                                        self.control.process = None
+            elif (uri == '/control/stop'):
+                print 'Shutting detection down..'
+                if (self.control.process is not None):
+                    self.control.process.kill()
+                    self.control.process = None
 
-                        elif (uri == '/control/snapshot'):
-                                if (self.control.process is not None):
-                                       httpClient = httpclient.HTTPClient() 
-                                       try:
-                                                response = httpClient.fetch('http://localhost:8080/0/action/snapshot')
-                                       except httpclient.HTTPERror, e:
-                                               print 'Error:', e
-                                       httpClient.close()
+            elif (uri == '/control/snapshot'):
+                if (self.control.process is not None):
+                    httpClient = httpclient.HTTPClient()
+                    try:
+                        httpClient.fetch('http://localhost:8080/0/action/'
+                                         + 'snapshot')
+                    except httpclient.HTTPERror, e:
+                        print 'Error:', e
+                        httpClient.close()
 
-                self.set_status(200)
-                self.finish()
+        self.set_status(200)
+        self.finish()
 
 
 if __name__ == "__main__":
-        
-        config = ConfigParser.ConfigParser()
-        config.read('key.cfg')
 
-        api_key = config.get('API', 'key') 
-        gcm = GCM(api_key)
-        control = ProcessControl()
-       
-        application = web.Application([
-                (r'/control/.*', MainHandler, dict(gcm = gcm, control = control)),
-                (r'/picture/(.*)', StaticFileHandler, { 'path' : 'picture' })
-                ],
-                )
+    config = ConfigParser.ConfigParser()
+    config.read('key.cfg')
 
-        application.listen(3030)
-        ioloop.IOLoop.instance().start()
+    api_key = config.get('API', 'key')
+    gcm = GCM(api_key)
+    control = ProcessControl()
+
+    application = web.Application([
+        (r'/control/.*', MainHandler, dict(gcm=gcm, control=control)),
+        (r'/picture/(.*)', StaticFileHandler, {'path': 'picture'})
+    ],)
+
+    application.listen(3030)
+    ioloop.IOLoop.instance().start()
