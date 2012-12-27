33c6c9e7f3c312698a992d63b126918d5d25a492
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-12-27 21:53:49 +0100

    Complete implementation for basic requests
    So far I have implemented now every request. I have also added a clean
    version of the motion configuration file. A local file is required,
    otherwise this application will not work properly.
    
    What is new is that the motion control port is now read from the motion
    configuration file itself allowing the user to change the control port
    without touching more than one file.
    
    The model is one class encapsulating all the functionality which is
    accessed by the Bottle route functions.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,19 +1,64 @@
 from bottle import get, post, request, run, static_file
+from gcm import GCM
+import fileinput
 import logging
+import subprocess
+import requests
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
 LOG.addHandler(logging.StreamHandler())
 
 
+class MotionProcess(object):
+    """Encapsulates the motion process handling"""
+
+    def __init__(self):
+        self.process = None
+        self.device = None
+        self.gcm = GCM('API_KEY')
+
+        for line in fileinput.input('motion.conf'):
+            split = line.split('control_port')
+            if len(split) is 2:
+                self.control_port = int(split[1])
+                LOG.debug('Found control port %d' % self.control_port)
+                break
+
+    def start(self):
+        if self.process is None and self.device is not None:
+            self.process = subprocess.Popen(['motion'])
+
+    def stop(self):
+        if self.process is not None:
+            self.process.kill()
+            self.process = None
+
+    def alert(self):
+        data = {}
+        self.gcm.plaintext_request(registration_id=self.device, data=data)
+
+    def set_device(self, id):
+        self.device = id
+
+    def request_snapshot(self):
+        if self.process is not None:
+            url = 'http://localhost:%d/0/action/snapshot' % self.control_port
+            requests.get(url)
+
+motion = MotionProcess()
+
+
 @get('/motion/detection/start')
 def start_motion_detection():
-    LOG.debug('Start Motion')
+    """Starts the motion process including detection for motion"""
+    motion.start()
 
 
 @get('/motion/detection/stop')
 def stop_motion_detection():
-    LOG.debug('Kill Motion')
+    """Stops the motion process"""
+    motion.stop()
 
 
 @get('/alerts/motion')
@@ -23,7 +68,8 @@ def notify_device_about_motion():
 
 @get('/server/action/snapshot')
 def make_snapshot():
-    LOG.debug('Makes a current snapshot')
+    LOG.debug('Request a current snapshot')
+    motion.request_snapshot()
 
 
 @get('/static/snapshots/<filename:re:.*\.jpg>')
@@ -35,12 +81,14 @@ def send_snapshot(filename):
 def register_device():
     id = request.forms.get('id')
     LOG.debug('Received id %s' % id)
+    motion.set_device(id)
 
 
 @post('/device/unregister')
 def unregister_device():
     id = request.forms.get('id')
     LOG.debug('Unregister device with id %s' % id)
+    motion.set_device(None)
 
 if __name__ == '__main__':
     run(host='localhost', port=4000)
