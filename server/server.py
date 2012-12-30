1894bb8ba2bd183f39821765322b139d86e5f5e9
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-12-30 03:54:52 +0100

    Check for authorization in each request
    There should be no request to the server which is not authorized. I have
    added a function decorator to perform the authentication. The
    credentials are given by a settings file. The decorator was added to
    each request.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
-from bottle import get, post, request, run, static_file
+from bottle import abort, get, post, request, run, static_file, parse_auth
 from gcm import GCM
+import ConfigParser
 import fileinput
 import logging
 import subprocess
@@ -46,49 +47,78 @@ class MotionProcess(object):
             url = 'http://localhost:%d/0/action/snapshot' % self.control_port
             requests.get(url)
 
+
+def read_settings(path):
+    config = ConfigParser.RawConfigParser()
+    config.read(path)
+    user = config.get('Authentification', 'user')
+    password = config.get('Authentification', 'password')
+    return {'user': user, 'password': password}
+
+
+settings = read_settings('settings.cfg')
+
+
+def authenticate(func):
+    def validate(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        if auth_header is None:
+            abort(401, 'Access denied')
+        credentials = parse_auth(auth_header)
+
+        if (credentials[0] == settings['user'] and
+                credentials[1] == settings['password']):
+            return func(**kwargs)
+        else:
+            abort(401, 'Access denied')
+
+    return validate
+
+
 motion = MotionProcess()
 
 
-@get('/motion/detection/start')
+@get('/motion/detection/start', apply=[authenticate])
 def start_motion_detection():
     """Starts the motion process including detection for motion"""
     motion.start()
 
 
-@get('/motion/detection/stop')
+@get('/motion/detection/stop', apply=[authenticate])
 def stop_motion_detection():
     """Stops the motion process"""
     motion.stop()
 
 
-@get('/alerts/motion')
+@get('/alerts/motion', apply=[authenticate])
 def notify_device_about_motion():
     LOG.debug('Notify device about motion')
 
 
-@get('/server/action/snapshot')
+@get('/server/action/snapshot', apply=[authenticate])
 def make_snapshot():
     LOG.debug('Request a current snapshot')
     motion.request_snapshot()
 
 
-@get('/static/snapshots/<filename:re:.*\.jpg>')
+@get('/static/snapshots/<filename:re:.*\.jpg>', apply=[authenticate])
 def send_snapshot(filename):
     return static_file(filename, root='snapshots', mimetype='image/jpg')
 
 
-@post('/device/register')
+@post('/device/register', apply=[authenticate])
 def register_device():
     id = request.forms.get('id')
     LOG.debug('Received id %s' % id)
     motion.set_device(id)
 
 
-@post('/device/unregister')
+@post('/device/unregister', apply=[authenticate])
 def unregister_device():
     id = request.forms.get('id')
     LOG.debug('Unregister device with id %s' % id)
     motion.set_device(None)
+
 
 if __name__ == '__main__':
     run(host='localhost', port=4000)
