6b0416e42ad7a1894c9a7caf2b1937daceaa989b
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-07 01:46:42 +0200

    Implement just-once instruction activity
    In order to avoid confusion about how this application works I have
    implemented a launcher activity which displays instructions. The user
    can either select 'Continue' or 'Exit'. If the user selects 'Continue'
    then the instruction will not be shown anymore.
    
    By using a translucent and no title bar theme the activity will not be
    noticed on further application starts.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- client/src/main/java/berlin/reiche/securitas/activities/LauncherActivity.java
+++ client/src/main/java/berlin/reiche/securitas/activities/LauncherActivity.java
@@ -1,8 +1,14 @@
 package berlin.reiche.securitas.activities;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
 import berlin.reiche.securitas.R;
 
 public class LauncherActivity extends Activity {
@@ -11,13 +17,60 @@ public class LauncherActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_launcher);
+		SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(this);
+
+		if (sp.getBoolean("skip", false)) {
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+			finish();
+		} else {
+			createInstructionDialog(sp).show();
+		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_launcher, menu);
-		return true;
+	public AlertDialog createInstructionDialog(final SharedPreferences sp) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Attention");
+		builder.setMessage("This application will not work without the "
+				+ "backend software. Make sure it is installed and running. "
+				+ "In the next step you will be asked to configure the endpoint.");
+
+		builder.setPositiveButton("Continue", new OnContinueListener(this, sp));
+		builder.setNegativeButton("Exit", new OnExitListener());
+		return builder.create();
+	}
+
+	private class OnContinueListener implements DialogInterface.OnClickListener {
+
+		Context context;
+		SharedPreferences sp;
+
+		public OnContinueListener(Context context, SharedPreferences sp) {
+			super();
+			this.context = context;
+			this.sp = sp;
+		}
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			Editor editor = sp.edit();
+			editor.putBoolean("skip", true);
+			editor.commit();
+
+			Intent i = new Intent(context, MainActivity.class);
+			context.startActivity(i);
+			finish();
+		}
+	}
+
+	private class OnExitListener implements DialogInterface.OnClickListener {
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			finish();
+		}
 	}
 
 }
