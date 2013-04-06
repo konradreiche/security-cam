0027ce2b27c285267d7b00bb592fe91eec9aab3f
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-06 02:33:48 +0200

    Create launcher activity
    One of the last features I want to implement before release on the
    Android application client is a launcher activity.
    
    This activity should serve as an entry point to launch either the main
    or settings activity, and, what is more, it should display instructions
    when the application is started for the first time. This way users will
    not download the application and think that it can work without the
    appropriate backend.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ client/src/berlin/reiche/securitas/LauncherActivity.java
@@ -0,0 +1,22 @@
+package berlin.reiche.securitas;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LauncherActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launcher);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_launcher, menu);
+		return true;
+	}
+
+}
