91fc1ef480f4e9276b0b15153d855949838bcc1e
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-09 01:19:06 +0200

    Document and refactor model classes.
    Cleaned up and documented the model classes. The previous state field
    was removed completely as it is not required anymore. For the time being
    the model is purely cosmetic as it does not implement any logic and the
    interface does not access it for retrieving the state. But, in order to
    be flexible for changes in the future this class will of course remain.
    After all this is a complete implementation of the MVC like architecture
    for Android applications. Also added more Javadoc.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

--- client/src/main/java/berlin/reiche/securitas/model/ClientModel.java
+++ client/src/main/java/berlin/reiche/securitas/model/ClientModel.java
@@ -3,47 +3,68 @@ package berlin.reiche.securitas.model;
 import android.graphics.Bitmap;
 import berlin.reiche.securitas.model.ClientModel.State;
 
+/**
+ * Concrete model for this application.
+ * 
+ * @author Konrad Reiche
+ * 
+ */
 public class ClientModel extends Model<State> {
 
+	/**
+	 * {@link Enum} state for the model. Either the detection is active
+	 * (detecting) or inactive (idle).
+	 */
 	public enum State {
 		IDLE, DETECTING;
 	}
 
-	protected State previous;
-
+	/**
+	 * Whether the device is registered on the backend server.
+	 */
 	private boolean registeredOnServer;
 
+	/**
+	 * The current snapshot taken on the backend server.
+	 */
 	private Bitmap snapshot;
 
+	/**
+	 * Default constructor.
+	 */
 	public ClientModel() {
 		super();
 		state = State.IDLE;
 	}
 
+	/**
+	 * @return whether the device is registered on the backend server.
+	 */
 	public boolean isRegisteredOnServer() {
 		return registeredOnServer;
 	}
 
-	@Override
-	public synchronized void setState(Enum<State> state) {
-		this.previous = (State) this.state;
-		super.setState(state);
+	/**
+	 * @param registeredOnServer
+	 *            whether the device is registered on the backend server.
+	 */
+	public void setRegisteredOnServer(boolean registeredOnServer) {
+		this.registeredOnServer = registeredOnServer;
 	}
 
-	public void setRegisteredOnServer(boolean flag) {
-		registeredOnServer = flag;
-	}
-
-	public void setSnapshot(Bitmap snapshot) {
-		this.snapshot = snapshot;
-	}
-
+	/**
+	 * @return the current snapshot taken on the backend server.
+	 */
 	public Bitmap getSnapshot() {
 		return snapshot;
 	}
 
-	public boolean isDetecting() {
-		return state == State.DETECTING;
+	/**
+	 * @param snapshot
+	 *            the current snapshot taken on the backend server.
+	 */
+	public void setSnapshot(Bitmap snapshot) {
+		this.snapshot = snapshot;
 	}
 
 }
--- client/src/main/java/berlin/reiche/securitas/model/Model.java
+++ client/src/main/java/berlin/reiche/securitas/model/Model.java
@@ -1,14 +1,31 @@
 package berlin.reiche.securitas.model;
 
-
+/**
+ * Representation of a generic model class as used in a MVC like architecture.
+ * 
+ * @author Konrad Reiche
+ * 
+ * @param <T>
+ *            {@link Enum} type of the model state.
+ */
 public abstract class Model<T extends Enum<T>> {
 
+	/**
+	 * Current state of the model.
+	 */
 	protected Enum<T> state;
 
+	/**
+	 * @return current state of the model.
+	 */
 	public synchronized Enum<T> getState() {
 		return state;
 	}
 
+	/**
+	 * @param state
+	 *            new state the model should transition to.
+	 */
 	public synchronized void setState(Enum<T> state) {
 		this.state = state;
 	}
