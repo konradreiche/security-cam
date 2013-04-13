ddeb5b093aafd2ad3216e87bb3c63ff91da4f5da
Konrad Reiche <konrad.reiche@gmail.com>
2013-04-13 22:27:52 +0200

    Wrap up server component for Python Package Index
    The Python Package Index (pypi) is a central repository for Python
    packages. I have wrapped up the server component. This required a
    setup.py file, some file moving and a MANIFEST file which files it
    should include.
    
    Signed-off-by: Konrad Reiche <konrad.reiche@gmail.com>

---
+++ server/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='security-cam',
+      version='0.1',
+      description='Attachment for Motion to set up a security system',
+      author='Konrad Reiche',
+      author_email='konrad.reiche@gmail.com',
+      url='http://www.konrad-reiche.com/security-cam',
+      license='mit',
+      packages=['securitas'],
+      keywords=['application', 'backend', 'motion'],
+      classifiers=[
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: Unix',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: System Administrators',
+          'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+          'Topic :: Multimedia :: Video'],
+      long_description="""A REST API on top of Motion.
+
+      Motion is a program that monitors the video signal from cameras. This
+      utility software is an attachment on top of Motion to offer a set of API
+      commands. These commands are thought to be used together with the client.
+      The client is an Android application which allows to remotely control the
+      motion detection and sends push notifications and snapshots the
+      client."""
+      )
